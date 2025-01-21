4cc92c570086d6aae0f4e99dc885cdc4d98bd787
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2025-01-21 20:39:59 +0000

    chore: deleted config.py from history

---
+++ data/config.py
@@ -0,0 +1,32 @@
+API_ID =  <api_id>
+
+API_HASH = <api_hash> # in quotes ""
+
+USE_TG_BOT = False
+
+# delay between connections to accounts
+ACC_DELAY = [5, 15]
+
+# folder with sessions (do not change)
+WORKDIR = "sessions/"
+
+# Using proxy
+USE_PROXY = False # True/False
+
+
+PROXY_TYPE = "socks5"
+
+# Full link to your Sessions folder
+SESSIONS_PATH = ""
+
+# mini sleep
+MINI_SLEEP = [3,7] #[min,max]
+
+hello ='''
+ █████╗ ███████╗███████╗██╗ █████╗
+██╔══██╗██╔════╝██╔════╝██║██╔══██╗
+███████║███████╗███████╗██║███████║
+██╔══██║╚════██║╚════██║██║██╔══██║
+██║  ██║███████║███████║██║██║  ██║
+╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═╝
+'''
